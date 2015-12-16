60439a83fa3b07b003b9ef5da6c7f368ff900aa4
Phippsy <donal.phipps@gmail.com>
2015-12-16 12:36:36 +0000

    First commit: wrote out pseudocode to set general structure, added in some function and variable placeholders.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a special matrix object which can cache its own inverse, using a cacheSolve function to get or set these values as required.
 
-## Write a short comment describing this function
-
+## Creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
+	# Create a null matrix to hold the inverse values
+    inv.mx<-
+	# A function to set the matrix values
+    set<-function()
+    # A function to get the matrix values
+    get<-function()
+    # A function to set the inverse matrix values
+    setInverse<-function()
+    # A function to get the inverse matrix values
+    getInverse<-function()
+    # Return each function above together as a list
 
 }
 
+## Computes the inverse of the special "matrix" returned by makeCacheMatrix
+cacheSolve <- function(x, ...) {        
+    # Assign the inverse matrix value currently held in x to a variable, inv.mx
+    inv.mx<-
+    # If the inverse matrix value is not null, return that value
+    if ( !is.null(inv.mx) ) {
+        # Send the user a message that the value is being retrieved.
+        message("Retrieving values from cache")
+    }
+    # If the inverse matrix value is null, compute the inverse and assign its' values to inv.mx
+        # Store the matrix in a local variable
+        mx<-
+        # Compute the inverse matrix values and write them back to x$setInverse
+        inv.mx<-solve()
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Return the values of the inverse matrix
+        inv.mx
 }
